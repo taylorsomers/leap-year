e0312555f8fb3f66be4b7f92733c7e39691165c2
Taylor Somers <tsomers@yandex.com>
2020-06-10 12:41:00 -0400

    Add further conditiont o return Boolean 'true' if year is divisible by 400, even though it is also divisible by both 4 and 100, which would produce 'false' if year is not divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic:
 
 function leapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
   } else {
     return false;
