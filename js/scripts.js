48c6aef63993cce65187249167fc27f8f707c0a9
Taylor Somers <tsomers@yandex.com>
2020-06-10 12:38:41 -0400

    Refactor business logic to simplify function leapYear(year) and reduce amount of code required to produce Boolean values in the same way performed so far

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 // Business Logic:
 
 function leapYear(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
     return true;
   } else {
     return false;
